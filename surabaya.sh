c0a78fcad76964ac00a7a7eb9f01182907df2983
Mubasir Alamsah <mubasiralamsah212@gmail.com>
2021-10-15 09:45:36 +0000

    surabaya.sh edited online with Bitbucket

--- surabaya.sh
+++ surabaya.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-if [ -z "$STY" ]; then exec screen -dm -S kuat /bin/bash "$0"; fi
-chmod +x kuat.sh
-./kuat.sh
+if [ -z "$STY" ]; then exec screen -dm -S jawa /bin/bash "$0"; fi
+chmod +x jawa.sh
+./jawa.sh
